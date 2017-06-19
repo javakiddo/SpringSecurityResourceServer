4e523fca119e72f26041ad96e60b7865c59ded50
kazi <kazi-Imran.Aziz@adviqo.com>
2017-06-19 16:18:53 +0200

    Controller Added

--- src/main/java/com/springsecurityangularjs/SpringSecurityResourceServerApplication.java
+++ src/main/java/com/springsecurityangularjs/SpringSecurityResourceServerApplication.java
@@ -6,13 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringSecurityResourceServerApplication
 {
 	
 	@RequestMapping("/")
-	//	@CrossOrigin(origins="*", maxAge=3600)
+	@CrossOrigin(origins="*", maxAge=3600)
 	public Message home()
 	{
 		return new Message("Hello World");
